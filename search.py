ca68797dd1a229914ad167218499f2d712607d80
singhamal001 <singhamal001@gmail.com>
2023-09-26 04:04:34 +0530

    Committing Linear Search code

---
+++ search.py
@@ -0,0 +1,68 @@
+from abc import ABC, abstractmethod
+import time
+import random
+
+#Base class for search 
+class Search(ABC):
+    """Abstract base class for searching."""
+
+    def __init__(self, items):
+        self._items = items
+
+    @abstractmethod
+    def perform_search(self):
+        pass
+
+#Sub Class for Linear Search.
+class LinearSearch(Search):
+    """Subclass for Linear Search."""
+
+    def __init__(self, items, target):
+        super().__init__(items)
+        self.target = target
+
+    # Doing Linear Search.
+    def perform_search(self):
+        start_time = time.time()
+        for index, item in enumerate(self._items):
+            if item == self.target:
+                end_time = time.time()
+                return index, start_time, end_time
+        end_time = time.time()
+        return -1, start_time, end_time
+
+
+if __name__ == "__main__":
+    # Specifying different array sizes.
+    data_sizes = [50, 500, 1000, 5000, 10000, 50000, 100000, 500000]
+    lin_search_times = []
+    bin_search_times = []
+
+    for size in data_sizes:
+        #Creating a list of random integers for different sizes of array
+        my_list = [random.randint(1, 1000) for i in range(size)]
+
+        #Getting the target to be found in the list
+        #Using Try Except block to validate the data type of the target.
+        while True:
+            try:
+                target = int(input("Enter the item that you want to search in the array: "))
+                break
+            except:
+                print("Please enter an integer as a target to be foundd in the list: ")
+
+        # Instantiating Linear Search
+        search_instance = LinearSearch(my_list, target)
+
+        # Performing the search and printing the result
+        result, s_ls_time, e_ls_time = search_instance.perform_search()
+        print(f"For the data size {size}")
+        if result != -1:
+            print(f"Item found in the array at index {result} \n")
+        else:
+            print("Item is not present in the array \n")
+
+        
+        ls_time = e_ls_time - s_ls_time
+        print(f"Time taken to complete the search for data size of {size} using Linear search is {ls_time:.15f}\n")
+        lin_search_times.append(ls_time)
